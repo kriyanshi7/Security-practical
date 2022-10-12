eb3a00664c1a4f8018453303380c53cc45e4f89e
kriyanshi7 <115484999+kriyanshi7@users.noreply.github.com>
2022-10-12 17:00:24 +0530

    Add files via upload

---
+++ Pactical_1/patched_code.py
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+  
+// Driver code
+int main()
+{
+    char temp[];
+    char str[] = "hello";
+    strlcpy(temp, str);
+    printf("%s", temp);
+    return 0;
+}
